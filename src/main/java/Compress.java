052088088504c7d54078bc9666d450abedee518f
Sebastian Lubiecki <sebastian.lubiecki@gmail.com>
2018-12-01 10:48:12 +0100

    Drugi

---
+++ src/main/java/Compress.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Compress {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static int SMSCompresion() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type your massage: \t");
+        String userText = scanner.nextLine();
+
+        String[] arrayOfUserText = userText.split(" ");
+        for (int i = 0; i < arrayOfUserText.length; i++) {
+            arrayOfUserText[i] = arrayOfUserText[i].substring(0, 1).toUpperCase() + arrayOfUserText[i].substring(1);
+            System.out.print(arrayOfUserText[i]);
+        }
+        char[] numberOfLetters = userText.trim().toCharArray();
+        return numberOfLetters.length;
+    }
+
+    public static double costOfSMS() {
+        double cost = 2.00;
+        int lengthOfSMS = SMSCompresion()/160;
+        double costOfSMS =  lengthOfSMS / cost;
+
+        return costOfSMS;
+    }
+
+}
